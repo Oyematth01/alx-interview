1577ce5c25495bfe8214a5e28bc8dae0ab4fc417
Oyematth01 <oyematth@gmail.com>
2024-10-07 00:25:09 +0100

    pascal_triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,21 @@
 def pascal_triangle(n):
-	if n == 0:
-		return []
-	if n == 1:
-		return [[1]]
-	
-	list_cont =[]
-	for i in range(n):
-		temp_list =[]
-		for j in range(i+1):
-			if j == 0 or j == i:
-				temp_list.append(1)
-			else:
-				temp_list.append(list_cont[i-1][j-1] + list_cont[i-1][j])
-		list_cont.append(temp_list)
-	print(list_cont)
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
+
+    list_cont = []
+    for i in range(n):
+        temp_list = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(list_cont[i - 1][j - 1] + list_cont[i - 1][j])
+        list_cont.append(temp_list)
+
+    return list_cont
 
 if __name__ == "__main__":
-    pascal_triangle(pascal_triangle(5))
+    result = pascal_triangle(5)
+    print(result)
